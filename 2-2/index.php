00633c729a5220312feaeaaf78ae25e3e192c908
juha38xyz <61047216+juha38xyz@users.noreply.github.com>
2020-10-06 14:05:40 +0300

    Update index.php

--- 2-2/index.php
+++ 2-2/index.php
@@ -1,1 +1,18 @@
 <?php
+
+class HelloWorld {
+    public string $nimi;
+    
+    protected int $maara = 100;
+   
+    private static float $hinta = 40.50;
+    
+
+   
+
+}
+$olio = new HelloWorld();
+
+var_dump($olio);
+
+
