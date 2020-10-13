1cef463180ad15bdb81838851def92f9d085c134
juha38xyz <61047216+juha38xyz@users.noreply.github.com>
2020-10-13 10:07:42 +0300

    Update index.php

--- 2-7/index.php
+++ 2-7/index.php
@@ -1,1 +1,28 @@
 <?php
+
+class Calculator{
+    public array $numbers;
+
+    public function addNumber(int $number): self
+    {
+        $this->numbers[] = $number;
+        //[5, 7]
+
+        return $this;
+
+    }
+
+    public function calculate()
+    {
+        $total = 0;
+
+        foreach($this->numbers as $number){
+            $total += $number;
+        }
+
+        return $total;
+    }
+}
+
+$calculator = new Calculator();
+$calculator->addNumber(5)->addNumber(7)->calculate();
