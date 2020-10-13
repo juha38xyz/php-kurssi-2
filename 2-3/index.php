0f0ec97db168fabe5082928ab7dd79982cca4f9e
juha38xyz <61047216+juha38xyz@users.noreply.github.com>
2020-10-13 09:48:01 +0300

    Update index.php

--- 2-3/index.php
+++ 2-3/index.php
@@ -1,1 +1,22 @@
 <?php
+
+class Movie {
+    //elukovan genre. esim. scifi, kauhu, komedia jne
+    public string $genre;
+    // elokuvan kesto tunneissa.
+    public float $kesto;
+    // elokuvan julkaisuvuosi
+    public int $vuosi;
+    //elokuvan ikäraja
+    public int $ikaraja;
+    // pääosan esittäjät
+    public array $nayttelijat;
+
+
+}
+$uuno = new Movie();
+$uuno->genre = 'Komedia';
+$uuno->kesto = 1.5;
+$uuno->vuosi = 1995;
+$uuno¨->ikaraja = 7;
+$uuno->nayttelijat = ['Vesku Loiri','Spede Pasanen'];
